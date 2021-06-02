1b90db6604095c66a31bb03cf57e12ad052078db
BombasticBipod <eric.schram3@gmail.com>
2021-06-01 21:40:00 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const { token } = require('./config/token.json');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(token);
+
