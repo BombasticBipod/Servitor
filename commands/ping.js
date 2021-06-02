9a079671556b8e4a95645cd76a7390b6a48d761e
BombasticBipod <eric.schram3@gmail.com>
2021-06-01 22:36:46 -0400

    Added command system
    Commands directory stores command files
    Dynamically imported and stored on discord client object

---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'ping',
+    description: 'echos the sent message or replies pong! if there is no content',
+    execute: (message) => {
+
+        let response_text = "pong!";
+        const message_content_no_prefix = message.content.slice(1).split(' ');
+        if(message_content_no_prefix.length > 1) {
+            response_text = message_content_no_prefix.slice(1).join(' ');
+        }
+        message.channel.send(response_text);
+    }
+}
--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const { token } = require('./config/token.json');
+const config = require('./config/data.json')
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
+    loadCommandFiles()
+        .then(res => console.log(res.message));
 });
+
+client.on('message', (message) => {
+    if(message.author.bot || !message.content.startsWith(config.prefix)) return;
+    const possible_prefix = message.content.slice(1).split(' ')[0];
+
+    if(client.commands.has(possible_prefix)) {
+        client.commands.get(possible_prefix).execute(message);
+    }
+})
+
+function loadCommandFiles() {
+    return new Promise((resolve) => {
+        client.commands = new Discord.Collection();
+        fs.readdirSync('./commands')
+            .filter(filename => filename.endsWith('.js'))
+            .map(filename => {
+                return require(`./commands/${filename}`);
+            })
+            .forEach(file => {
+                client.commands.set(file.name, file);
+            });
+        resolve({
+            message: [...client.commands.keys()].reduce((str, key) => str += `\t${client.commands.get(key).name}\n`, "Successfully loaded:\n")
+        });
+    })
+}
 
 client.login(token);
 
